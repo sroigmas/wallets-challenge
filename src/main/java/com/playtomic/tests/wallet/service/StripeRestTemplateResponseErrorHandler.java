c90bbccceaf5680d64eb86d6a3d90a4a62ed99aa
Salva Roig <salroima@gmail.com>
2024-04-07 18:44:19 +0200

    feat(challenge): Update to Java 21 and Spring Boot 3.2.4

--- src/main/java/com/playtomic/tests/wallet/service/StripeRestTemplateResponseErrorHandler.java
+++ src/main/java/com/playtomic/tests/wallet/service/StripeRestTemplateResponseErrorHandler.java
@@ -1,19 +1,20 @@
 package com.playtomic.tests.wallet.service;
 
+import java.io.IOException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.DefaultResponseErrorHandler;
 
-import java.io.IOException;
-
 public class StripeRestTemplateResponseErrorHandler extends DefaultResponseErrorHandler {
 
-    @Override
-    protected void handleError(ClientHttpResponse response, HttpStatus statusCode) throws IOException {
-        if (statusCode == HttpStatus.UNPROCESSABLE_ENTITY) {
-            throw new StripeAmountTooSmallException();
-        }
-
-        super.handleError(response, statusCode);
+  @Override
+  protected void handleError(ClientHttpResponse response, HttpStatusCode statusCode)
+      throws IOException {
+    if (statusCode == HttpStatus.UNPROCESSABLE_ENTITY) {
+      throw new StripeAmountTooSmallException();
     }
+
+    super.handleError(response, statusCode);
+  }
 }
