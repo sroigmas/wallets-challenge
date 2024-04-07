402e76fcf289a2400498f9305db80486bc7b0936
Salva Roig <salroima@gmail.com>
2024-04-07 19:55:25 +0200

    feat(challenge): Add domain

---
+++ src/main/java/com/playtomic/tests/wallet/domain/Wallet.java
@@ -0,0 +1,17 @@
+package com.playtomic.tests.wallet.domain;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Wallet {
+  private UUID id;
+  private BigDecimal balance;
+}
